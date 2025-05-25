202116c937dea1f853dc1f8f9a8936645aee6d9e
SLK gaming <dangcongthanhdat1@gmail.com>
2025-05-25 13:51:16 +0700

    Create Hutao-Hub-V4.lua

---
+++ Hutao-Hub-V4.lua
@@ -0,0 +1,5 @@
+-- Made by SLK gaming
+-- What are you doing here ?
+-- You shouldn't be here...
+-- Go away Skid 🤬 !
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SLK-gaming/Dead-Rails/refs/heads/main/Hutao-Hub-Anti-Robot.txt"))()
